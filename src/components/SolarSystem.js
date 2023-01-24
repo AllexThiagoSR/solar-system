056122b6c7c993fab49091cd1f777b01a23e6c18
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-24 15:52:43 -0300

    Requisito 6: Renderiza a lista de planetas

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
+
+const makeAPlanetCard = ({ name, image }) => (
+  <PlanetCard planetName={ name } planetImage={ image } key={ name } />
+);
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { Planets.map(makeAPlanetCard) }
       </div>
     );
   }
