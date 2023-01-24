72d58e11d029bf8962ea781c0baecee337d0e6cc
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-01-24 16:34:05 -0300

    Requisito 10: Renderiza a lista de missões

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missionsList from '../data/missions';
+
+const makeAMissionCard = ({ name, country, year, destination }) => (
+  <MissionCard
+    key={ name }
+    name={ name }
+    country={ country }
+    year={ year }
+    destination={ destination }
+  />
+);
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        <section className="missions-sec">
+          { missionsList.map(makeAMissionCard) }
+        </section>
       </div>
     );
   }
